6e7edd9e112ee755e08290144ede663911cef413
AceDZN <design@acedzn.com>
2016-05-22 16:38:03 +0300

    + Added github form component * some styling

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,10 +1,13 @@
 import React,{ Component } from 'react';
+import ForkBanner from './fork-banner';
 import PlatformList from '../containers/platforms-list';
 import GreetMessage from '../containers/greet-message';
+
 export default class App extends Component {
   render() {
     return (
       <div>
+        <ForkBanner />
         <GreetMessage />
         <PlatformList />
       </div>
